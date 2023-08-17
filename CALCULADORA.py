995b5cbc5f97db81bc5bb72348e7256defab4bf3
Jhon171819 <141761671+Jhon171819@users.noreply.github.com>
2023-08-17 13:18:42 -0300

    Update CALCULADORA.py

--- CALCULADORA.py
+++ CALCULADORA.py
@@ -1,16 +1,16 @@
 import PySimpleGUI as sg
-
+Re = 0
 sg.theme('DarkAmber')
 sg.set_options(font='Calibri 25')
 numero = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-num = []
-full_op= []
+num = ''
+full_op= ''
 layout = [
     [sg.Txt('calculadora em Python', justification='right', expand_x=True,key='-text-')],
     [sg.Button('Limpar', expand_x=True)],
     [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('4'), sg.Button('5'), sg.Button('+')],
     [sg.Button('6'), sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('0'), sg.Button('-')],
-    [sg.Button('X'), sg.Button('/')],
+    [sg.Button('*'), sg.Button('/')],
     [sg.Button('Enter', expand_x= True)]
 ]
 
@@ -20,29 +20,41 @@ while True:
     events, values = janela.read()
 
     if events in numero:
-        num.append(events)
-        num_string = ''.join(num)
-        janela['-text-'].update(num_string)
+        num+= events
+        num_string = num
+
+        janela['-text-'].update(f'{full_op} {num_string}')
+
 
     if events == 'Limpar':
-        num = []
-        num_string =[]
-        full_op = []
+        num = ''
+        num_string =''
+        full_op = ''
         janela['-text-'].update('   ')
         janela.read()
 
 
-    if events in ['-','+','X','/']:
-        full_op = []
-        full_op.append(''.join(num_string))
-        num = []
-        janela['-text-'].update('')
+    if events in ['-','+','*','/']:
+        full_op+= num
+        num= ''
+        full_op+=events
+        
+
+        janela['-text-'].update(full_op)
+
 
 
     if events == 'Enter':
-        full_op.append(''.join(num))
+        full_op+=num
 
-        result = (full_op)
-        janela['-text-'].update(result)
+
+        
+
+        result = eval((full_op))
+
+        janela['-text-'].update('{:.0f}'.format(result))
+
+    if events == sg.WINDOW_CLOSED:
+        break
 
 
